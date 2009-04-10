ab4ce3e7b45186433db2c3408d2878e87e3e906f
svelikov <svelikov@17868f16-253a-11de-a101-5ba453ef2bb8>
2009-04-10 22:43:09 +0000

    added support for all signs from 32-1103 UTF8 and fixed algorithm for hyphenation

--- hardhyphenation.js
+++ hardhyphenation.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Plugin that is used for hard hyphenation of too long strings that doesn't fit
  * to the width of the wrapper component.
  */
@@ -6,9 +6,6 @@ Hyphenation = {
 
 	// array containing all the elements to be processed
 	previewElements :null,
-
-	// font size that is set on body - calculated bellow
-	// fSize : 0,
 
 	// letter-spacing in pixels
 	LETT_SPACE :0,
@@ -46,7 +43,7 @@ Hyphenation = {
 	findAndFix : function(hyphenType, selector) {
 		if (hyphenType) {
 			this.HYPHEN_TYPE = hyphenType;
-			this.HYPHEN_TYPE_WIDTH = signs[hyphenType.charCodeAt(0)];
+			this.HYPHEN_TYPE_WIDTH = signs[hyphenType.charCodeAt(0) - 32];
 		} else {
 			return;
 		}
@@ -57,7 +54,6 @@ Hyphenation = {
 		}
 
 		this.previewElements = jQuery(this.SELECTOR);
-		// this.fSize = this.getFontSize(document.body);
 		if (this.previewElements.length > 0) {
 			this.checkContent();
 		}
@@ -78,18 +74,18 @@ Hyphenation = {
 			currentCell = this.previewElements[i];
 			txt = currentCell.innerHTML;
 			// call a method to trim left and right the text
-			txt = txt.trim();
+			//txt = txt.trim();
 			// get the width in pixels of the current element
 			parentWidth = this.previewElements[i].parentNode.offsetWidth;
 			// call the method to hyphenate the text if needed
-			fixedString = this.breakString(txt, parentWidth
-					- this.HYPHEN_TYPE_WIDTH);
+			fixedString = this.breakString(txt, parentWidth - this.HYPHEN_TYPE_WIDTH);
+			// put back the returned string into its container
 			currentCell.innerHTML = fixedString;
 		}
 	},
 
 	/**
-	 * Parses the provided string and inserts a breaking character (hyphen or
+	 * Parces the provided string and inserts a breaking character (hyphen or
 	 * space) on place where string is to overrun the provided width.
 	 * 
 	 * @param txt
@@ -106,23 +102,31 @@ Hyphenation = {
 		for ( var i = 0; i < txtLength; i++) {
 			var ch = txt.substring(i, i + 1);
 			var wbr = 0;
+			// if current sign is a word-breacking one we notice that in 
+			// order to know whether to insert breaking char when we reach
+			// to the end of the line (parentWidth < sum)
 			if ((ch == ' ') || (ch == '-')) {
 				wbr = 1;
-				sum = 0;
 			}
 			// if calculated sum is to overrun the width we need to hyphenate
-			if (sum >= parentWidth) {
+			// but if we have found a breacking char earlier on the same row
+			// we doesn't inject a breaking char and rely on the browser to
+			// hyphenate 
+			// FIXME there is an issue under FF3 where it doesn't hyphenate properly
+			if (parentWidth < sum) {
+				// if there isn't breaking char on the row we apply hard hyphenation
 				if (wbr == 0) {
 					// insert breacking character
 					resultStr += this.HYPHEN_TYPE;
-					// clear the sum to start a new 'line'
+					// clear the sum to start a new row
 					sum = 0;
 				}
 			}
-			// increment the sum with the width of the current sign
-			// 32 - ascii for 'space', 126 - ascii for '~'
-			if (ch.charCodeAt(0) >= 32 && ch.charCodeAt(0) <= 126) {
-				sum += signs[ch.charCodeAt(0) - 32] + this.LETT_SPACE;
+			
+			// 32 - UTF8 for 'space', 1103 - UTF8 for 'who knows'
+			if (ch.charCodeAt(0) >= 32 && ch.charCodeAt(0) <= 1103) {
+				// increment the sum with the width of the current sign + letter space
+				sum += allSigns[ch.charCodeAt(0) - 32] + this.LETT_SPACE;
 			}
 
 			// append the current char to the resulting string
@@ -148,15 +152,23 @@ Hyphenation = {
 	 * Helper function that may be used to calculate the letters width.
 	 */
 	calculateWidth : function() {
-		var el = document.getElementById('lett');
-		var el2 = document.getElementById('arr');
-		var chDiv = document.getElementById('ch');
-		var arr = new Array();
-		for (i = 32; i <= 126; i++) {
-			el.innerHTML = String.fromCharCode(i);			
-			arr[i] = el.offsetWidth;
-			el2.innerHTML += (String.fromCharCode(i) + '=' + arr[i] + ', ');
-			chDiv.innerHTML += String.fromCharCode(i) + ', ';
+		var ch = document.getElementById('ch');
+		var chWidth = 0;
+		for (i = 32; i <= 1103; i++) {
+			ch.innerHTML = String.fromCharCode(i);
+			chWidth = ch.offsetWidth;
+			this.print(chWidth + ', ', 1);
+		}
+	},
+	
+	/**
+	 * Helper method that prints in the html page.
+	 */
+	print : function (txt, append) {
+		if (append) {
+			document.getElementById('print').innerHTML += txt;
+		} else {
+			document.getElementById('print').innerHTML = txt;
 		}
 	}
 }
@@ -176,4 +188,11 @@ var signs = new Array(8, 3, 4, 7, 7, 11, 8, 2, 4, 4, 5, 7, 3, 4, 3, 3, 7, 7,
 		7, 7, 7, 7, 7, 7, 7, 7, 3, 3, 7, 7, 7, 7, 12, 7, 8, 9, 9, 8, 7, 9, 9,
 		3, 6, 8, 7, 9, 9, 9, 8, 9, 9, 8, 7, 9, 7, 11, 7, 7, 7, 3, 3, 3, 5, 7,
 		4, 7, 7, 6, 7, 7, 3, 7, 7, 3, 3, 6, 3, 11, 7, 7, 7, 7, 4, 7, 3, 7, 5,
-		9, 5, 5, 5, 4, 3, 4, 7);
+		9, 5, 5, 5, 4, 3, 4, 7,
+		7, 8, 8, 7, 8, 8, 11, 7, 9, 9, 7, 8, 9, 9, 9, 9, 8, 9, 7, 8, 9, 7, 9,
+		8, 11, 11, 10, 10, 8, 9, 12, 9, 7, 7, 6, 4, 7, 7, 9, 6, 7, 7, 6, 7, 9,
+		7, 7, 7, 7, 6, 5, 5, 9, 5, 7, 6, 9, 9, 8, 9, 7, 6, 9);
+
+// the widths of the signs from 32 to 1103 UTF8 when font-size = 12px
+var allSigns = new Array(0, 3, 4, 7, 7, 11, 8, 2, 4, 4, 5, 7, 3, 4, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 3, 3, 7, 7, 7, 7, 12, 7, 8, 9, 9, 8, 7, 9, 9, 3, 6, 8, 7, 9, 9, 9, 8, 9, 9, 8, 7, 9, 7, 11, 7, 7, 7, 3, 3, 3, 5, 7, 4, 7, 7, 6, 7, 7, 3, 7, 7, 3, 3, 6, 3, 11, 7, 7, 7, 7, 4, 7, 3, 7, 5, 9, 5, 5, 5, 4, 3, 4, 7, 13, 13, 13, 13, 13, 13, 0, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 3, 3, 7, 7, 7, 7, 3, 7, 4, 9, 4, 7, 7, 4, 9, 7, 5, 7, 4, 4, 4, 7, 6, 3, 4, 4, 4, 7, 10, 10, 10, 7, 7, 7, 7, 7, 7, 7, 12, 9, 8, 8, 8, 8, 3, 3, 3, 3, 9, 9, 9, 9, 9, 9, 9, 7, 9, 9, 9, 9, 9, 7, 8, 8, 7, 7, 7, 7, 7, 7, 11, 6, 7, 7, 7, 7, 3, 3, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 5, 7, 5, 7, 7, 7, 7, 7, 7, 9, 6, 9, 6, 9, 6, 9, 6, 9, 7, 9, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 9, 7, 9, 7, 9, 7, 9, 7, 9, 7, 9, 7, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 9, 5, 6, 3, 8, 6, 7, 7, 3, 7, 3, 7, 4, 7, 4, 7, 3, 9, 7, 9, 7, 9, 7, 7, 9, 7, 9, 7, 9, 7, 9, 7, 12, 11, 9, 4, 9, 4, 9, 4, 8, 7, 8, 7, 8, 7, 8, 7, 7, 3, 7, 5, 7, 3, 9, 7, 9, 7, 9, 7, 9, 7, 9, 7, 9, 7, 11, 9, 7, 5, 7, 7, 5, 7, 5, 7, 5, 3, 7, 9, 8, 7, 8, 7, 9, 9, 6, 9, 10, 8, 7, 7, 8, 9, 8, 7, 7, 9, 8, 11, 3, 3, 8, 6, 3, 6, 10, 9, 7, 9, 10, 8, 11, 8, 9, 7, 8, 8, 6, 7, 5, 3, 7, 3, 7, 10, 8, 9, 9, 9, 6, 7, 6, 6, 7, 6, 6, 7, 7, 5, 6, 7, 3, 3, 9, 3, 16, 14, 12, 13, 9, 5, 14, 11, 9, 7, 7, 3, 3, 9, 7, 9, 7, 9, 7, 9, 7, 9, 7, 9, 7, 7, 7, 7, 7, 7, 12, 11, 8, 7, 8, 7, 7, 6, 9, 7, 9, 7, 6, 5, 3, 16, 14, 12, 9, 7, 13, 7, 9, 7, 7, 7, 12, 11, 9, 7, 7, 7, 7, 7, 8, 7, 8, 7, 3, 3, 3, 3, 9, 7, 9, 7, 9, 4, 9, 4, 9, 7, 9, 7, 7, 5, 8, 5, 6, 5, 9, 7, 8, 13, 7, 6, 7, 6, 7, 7, 7, 7, 9, 7, 9, 7, 9, 7, 9, 7, 7, 5, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 7, 7, 7, 7, 6, 6, 7, 7, 7, 7, 9, 6, 6, 8, 6, 4, 7, 7, 7, 6, 7, 7, 7, 7, 3, 3, 3, 4, 3, 3, 7, 10, 10, 10, 7, 7, 7, 7, 9, 8, 7, 4, 4, 4, 4, 4, 3, 3, 6, 6, 6, 3, 4, 3, 5, 3, 3, 7, 7, 6, 6, 9, 6, 6, 6, 7, 5, 6, 6, 6, 6, 6, 9, 7, 6, 7, 7, 5, 6, 5, 7, 6, 6, 11, 11, 12, 9, 5, 9, 9, 8, 7, 6, 7, 13, 13, 5, 5, 3, 3, 3, 4, 4, 6, 4, 7, 6, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 4, 4, 3, 4, 5, 7, 3, 7, 7, 7, 5, 5, 3, 3, 6, 6, 6, 6, 4, 4, 4, 4, 4, 4, 3, 13, 5, 2, 4, 5, 4, 5, 5, 5, 5, 5, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 0, 0, 0, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 4, 4, 13, 13, 13, 13, 7, 13, 13, 13, 3, 13, 13, 13, 13, 13, 4, 4, 7, 3, 9, 10, 4, 13, 9, 13, 9, 9, 3, 7, 8, 7, 7, 8, 7, 9, 9, 3, 8, 7, 9, 9, 8, 9, 9, 8, 13, 7, 7, 7, 9, 7, 9, 9, 3, 7, 7, 5, 7, 3, 7, 7, 7, 5, 7, 5, 5, 7, 7, 3, 7, 5, 7, 5, 5, 7, 8, 7, 6, 7, 5, 7, 8, 6, 9, 9, 3, 7, 7, 7, 9, 13, 7, 7, 9, 11, 9, 7, 8, 7, 9, 7, 9, 6, 7, 6, 8, 6, 9, 7, 10, 10, 8, 7, 8, 6, 8, 8, 7, 7, 9, 7, 6, 5, 7, 7, 6, 3, 9, 6, 6, 13, 13, 13, 13, 13, 13, 13, 13, 13, 7, 8, 10, 7, 9, 8, 3, 3, 6, 13, 12, 10, 7, 8, 8, 9, 7, 8, 8, 7, 8, 8, 11, 7, 9, 9, 7, 8, 9, 9, 9, 9, 8, 9, 7, 8, 9, 7, 9, 8, 11, 11, 10, 10, 8, 9, 12, 9, 7, 7, 6, 4, 7, 7, 9, 6, 7, 7, 6, 7, 9, 7, 7, 7, 7, 6, 5, 5, 9, 5, 7, 6, 9, 9, 8, 9, 7, 6, 9, 7);
+		
